7a5730457b03e4560322fb4e5b263bb77a5e713f
Fotis <fotisz@users.noreply.github.com>
2017-11-09 13:10:56 +0000

    Update import.js

--- import.js
+++ import.js
@@ -21,7 +21,7 @@
 // be displayed, as they were not annotated.
 
 var rootId = 8;
-var apiPath = "https://api.brain-map.org/api/v2/";
+var apiPath = "http://api.brain-map.org/api/v2/";
 var structureGraphId = 1; 
 
 function importData(id, callback) {
